1554203acab0a71ee9c440629213aa1a1ade7c3d
TaisF <115324446+TaisF@users.noreply.github.com>
2022-10-07 19:09:41 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true
+});
